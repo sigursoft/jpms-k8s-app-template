a04f217a9cd7c4e0aa14b3709ef9643aa7bd3ea7
Anton Kozik <anton.kozik@gmail.com>
2020-04-05 00:23:16 +0200

    add joda-money dependency

--- jpms-k8s-app/src/main/java/com/sigursoft/jpms/k8s/app/Application.java
+++ jpms-k8s-app/src/main/java/com/sigursoft/jpms/k8s/app/Application.java
@@ -1,5 +1,7 @@
 package com.sigursoft.jpms.k8s.app;
 
+import org.joda.money.Money;
+
 import java.util.logging.Logger;
 
 public class Application {
@@ -7,7 +9,9 @@ public class Application {
     private static final Logger LOGGER = Logger.getLogger(Application.class.toString());
 
     public static void main(String[] args) {
+        Money salary = Money.parse("USD 10000");
         LOGGER.info("Hello From JPMS K8s sample application.");
+        LOGGER.info(String.format("Here is another log message. %s", salary.toString()));
     }
 
 }
--- jpms-k8s-app/src/main/java/module-info.java
+++ jpms-k8s-app/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module jpms.k8s.app {
+    // safe money store
+    requires org.joda.money;
     // logging
     requires java.logging;
 }
