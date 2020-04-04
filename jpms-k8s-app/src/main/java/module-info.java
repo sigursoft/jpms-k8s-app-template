c7192315dd85aafec8e133e39c7a5f6bff50339f
Anton Kozik <anton.kozik@grodan.com>
2020-04-04 21:10:55 +0200

    add moditect-maven-plugin

--- jpms-k8s-app/src/main/java/module-info.java
+++ jpms-k8s-app/src/main/java/module-info.java
@@ -1,10 +1,4 @@
 module jpms.k8s.app {
-    // jdbc utility
-    requires commons.dbutils;
-    // jdbc driver
-    requires org.mariadb.jdbc;
     // logging
     requires java.logging;
-    // SQL
-    requires java.sql;
 }
